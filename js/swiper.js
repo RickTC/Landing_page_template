87790d9f3f09640c8789e9d40d7f386d49594cfc
Ricardo Tapia <rtapia@121corp.com>
2022-06-01 16:20:20 -0500

    se ajustan parametros de menu y slide con autoplay

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     mousewheel: true,
     keyboard: true,
